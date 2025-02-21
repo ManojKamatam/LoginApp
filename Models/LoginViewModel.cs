d3e0edc12c8c76c33c274e2b137c9858054f85b4
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2025-02-21 20:24:57 +0530

    Update LoginViewModel.cs

--- Models/LoginViewModel.cs
+++ Models/LoginViewModel.cs
@@ -5,12 +5,10 @@ namespace LoginApp.Models
     public class LoginViewModel
     {
         [Required(ErrorMessage = "Username is required")]
-        public string Username { get; set; } = string.Empty;
-
+        public string Username { get; set; }
+    
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
-        public string Password { get; set; } = string.Empty;
-
-        public bool RememberMe { get; set; }
+        public string Password { get; set; }
     }
 }
